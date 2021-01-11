435d94cb28c8078a14877c13f92353d2520e2251
marakeby <marakeby@vt.edu>
2021-01-11 06:51:47 -0500

    clean code

--- preprocessing/clean_text.py
+++ preprocessing/clean_text.py
@@ -1,23 +1,32 @@
 import re
+from copy import copy
+
 import numpy as np
 import logging
 
 from sklearn.base import BaseEstimator
 
 
-def preprocess_text(sen):
+def preprocess_text(sen, remove_tag=True, remove_special_chars=True, remove_numbers=False, remove_single_chars=True, remove_multiple_sapces=True):
+    sentence =copy(sen)
     # Removing html tags
-    sentence = remove_tags(sen)
+    if remove_tag:
+        sentence = remove_tags(sen)
 
     # Remove punctuations and numbers
-    # sentence = re.sub('[^a-zA-Z]', ' ', sentence)
-    sentence = re.sub('[^0-9a-zA-Z:,]', ' ', sentence)
+    if remove_special_chars:
+        sentence = re.sub('[^0-9a-zA-Z:,]', ' ', sentence)
 
-    # Single character removal
-    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+    if remove_numbers:
+        sentence = re.sub('[^a-zA-Z]', ' ', sentence)
 
-    # Removing multiple spaces
-    sentence = re.sub(r'\s+', ' ', sentence)
+    if remove_single_chars:
+        # Single character removal
+        sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+
+    if remove_multiple_sapces:
+        # Removing multiple spaces
+        sentence = re.sub(r'\s+', ' ', sentence)
 
     return sentence
 
@@ -27,8 +36,9 @@ def remove_tags(text):
     return TAG_RE.sub('', text)
 
 class CleanText(BaseEstimator):
-    def __init__(self):
+    def __init__(self, lower_case =False):
         logging.info('preprocessing: cleaning text')
+        self.lower_case= lower_case
 
 
     def fit(self, X):
@@ -38,6 +48,8 @@ class CleanText(BaseEstimator):
         ret_list = []
         sentences = list(X)
         for sen in sentences:
+            if self.lower_case:
+                sen = sen.lower()
             ret_list.append(preprocess_text(sen))
         return np.array(ret_list)
 
