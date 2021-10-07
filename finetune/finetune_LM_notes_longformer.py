903469cc1b1bd454f54c1664511c00d46a39dfdd
marakeby <marakeby@vt.edu>
2021-10-07 19:40:16 +0000

    truncate data from the beginning for fine tuning
    truncate data from the beginning for fine tuning

--- finetune/finetune_LM_notes_longformer.py
+++ finetune/finetune_LM_notes_longformer.py
@@ -6,73 +6,76 @@ from datasets import load_dataset, Dataset
 from transformers import DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
 
-
 import torch
-
-torch.cuda.empty_cache()
 torch.manual_seed(0)
 
 
-# bert_model_tiny = 'google/bert_uncased_L-2_H-128_A-2'
-# bert_model_mini = 'google/bert_uncased_L-4_H-256_A-4'
-# bert_model_med = 'google/bert_uncased_L-8_H-512_A-8'
-# bert_model_base = 'google/bert_uncased_L-12_H-768_A-12'
-longformer = 'allenai/longformer-base-4096'
+bert_model_tiny = 'google/bert_uncased_L-2_H-128_A-2'
+bert_model_mini = 'google/bert_uncased_L-4_H-256_A-4'
+bert_model_med = 'google/bert_uncased_L-8_H-512_A-8'
+bert_model_base = 'google/bert_uncased_L-12_H-768_A-12'
+longformer_model = 'allenai/longformer-base-4096'
 
-# model_ext = 'base'
-model_ext = 'longformer'
-model_name = longformer
-# checkpoint_path = '/home/haithamelmarakeby/logs/tiny_bert_tuned_notes_output_valid/checkpoint-40000'
-# checkpoint_path = '/home/haithamelmarakeby/logs/med_bert_tuned_notes_output/checkpoint-320000'
-bert_tiny = AutoModelForMaskedLM.from_pretrained(model_name)
-
-input_dir = 'gs://profile-notes/geekfest_files/unlabeled_data/'
+models_dict= {'mini': bert_model_mini, 'med': bert_model_med, 'tiny': bert_model_tiny, 'base':bert_model_base, 'longformer': longformer_model}
 
 train_filename = 'train_text_imaging_only.txt'
 valid_filename = 'valid_text_imaging_only.txt'
 
+input_dir = 'gs://profile-notes/geekfest_files/unlabeled_data/' #directory where input data is stored
+base_dir = '/home/haithamelmarakeby/pretrained_models_truncated' #used save trained model, checkpoints, and logs
+
+def truncate(x, n):
+    ret = [x[0]] + x[-n:]
+    return ret
 
 class TorchDataset(torch.utils.data.Dataset):
-    def __init__(self, text, tokenizer):
+    def __init__(self, text, tokenizer, max_length):
         self.text = text
         self.tokenizer = tokenizer
+        self.max_length = max_length
         
     def __getitem__(self, idx):
         item = self.text[idx]
-        item_tokenized = self.tokenizer(item, truncation=True, padding=True, return_special_tokens_mask=True)
-        ret = {key: torch.tensor(val) for key, val in item_tokenized.items()}
+        item_tokenized = self.tokenizer.batch_encode_plus([item], max_length= self.max_length, padding='max_length',
+                                                  truncation=False, return_special_tokens_mask=True, pad_to_max_length = True)
+
+        trucated_tokens = {key: truncate(val[0], self.max_length - 1) for key, val in item_tokenized.items()}
+        ret = {key: torch.tensor(val) for key, val in trucated_tokens.items()}
+
         return ret
 
     def __len__(self):
         return len(self.text)
-    
 
-def run(valid):
-    if valid == True:
-        filename= join(input_dir,valid_filename)
-        output_dir = "/home/haithamelmarakeby/logs/{}_bert_tuned_notes_output_valid".format(model_ext)
-        logging_dir = "/home/haithamelmarakeby/logs/{}_bert_tuned_notes_log_valid".format(model_ext),
-        saving_dir = '/home/haithamelmarakeby/pretrained_models/{}_bert_tuned_notes_valid'.format(model_ext)
+def run(bert_model_name='tiny', dataset = 'valid', max_len=512):
+    sub_dir = join(base_dir, bert_model_name)
+    model_name = models_dict[bert_model_name]
+    bert_model = AutoModelForMaskedLM.from_pretrained(model_name)
+
+    if dataset == 'valid':
+        input_filename= valid_filename
+        sub_dir = join(sub_dir, 'validation')
     else:
-        filename= join(input_dir,train_filename)
-        output_dir = "/home/haithamelmarakeby/logs/{}_bert_tuned_notes_output".format(model_ext)
-        logging_dir = "/home/haithamelmarakeby/logs/{}_bert_tuned_notes_log".format(model_ext),
-        saving_dir = '/home/haithamelmarakeby/pretrained_models/{}_bert_tuned_notes'.format(model_ext)
+        input_filename= train_filename
+        sub_dir = join(sub_dir, 'train')
 
-    train_data =  pd.read_csv(filename)
+    output_dir = join(sub_dir, 'output')
+    logging_dir = join(sub_dir, 'log')
+    saving_dir = join(sub_dir, 'model')
+
+    train_data =  pd.read_csv(join(input_dir,input_filename) )
     train_data = train_data[train_data.text.str.len() > 50]
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name, truncation=True, padding=True, max_len=1024)
-
+    # tokenizer = AutoTokenizer.from_pretrained(model_name, truncation=True, padding=True, max_len=512)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, truncation=True, padding=True, max_len=max_len)
 
     data_collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer, mlm=True, mlm_probability=0.15
     )
 
 
-    input_dataset = TorchDataset(train_data['text'].values, tokenizer)
+    input_dataset = TorchDataset(train_data['text'].values, tokenizer, max_length=max_len)
 
-    model = bert_tiny
     training_args = TrainingArguments(
         output_dir= output_dir,
         logging_dir =logging_dir,  
@@ -96,10 +99,9 @@ def run(valid):
 
     trainer.train()
 
-    
-    bert_tiny.save_pretrained(saving_dir)
+    bert_model.save_pretrained(saving_dir)
     print ('saved_file {}'.format(saving_dir))
 
 if __name__=="__main__":
     #use validartion data only 
-    run(valid=False)
+    run(bert_model_name='longformer', dataset = 'train', max_len=1024)
